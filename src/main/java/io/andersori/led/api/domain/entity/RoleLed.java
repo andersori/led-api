316e9c3fa345d5f41dda19cfb9e6451f34ffd666
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2019-11-19 11:29:12 -0300

    constraint errors fixed

--- src/main/java/io/andersori/led/api/domain/entity/RoleLed.java
+++ src/main/java/io/andersori/led/api/domain/entity/RoleLed.java
@@ -1,5 +1,5 @@
 package io.andersori.led.api.domain.entity;
 
 public enum RoleLed {
-	DEFAULT, ADMIN, TEACHER, TEAM;
+	DEFAULT, ADMIN, TEACHER;
 }
--- src/main/java/io/andersori/led/api/domain/policy/RestrictionHandler.java
+++ src/main/java/io/andersori/led/api/domain/policy/RestrictionHandler.java
@@ -6,19 +6,13 @@ import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.andersori.led.api.app.web.util.AccountContext;
 import io.andersori.led.api.domain.entity.RoleLed;
 import io.andersori.led.api.domain.exception.MethodNotAllowedException;
 
 public class RestrictionHandler implements InvocationHandler {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(RestrictionHandler.class);
-	
+		
 	private final Object target;
 	private final Class<?> classTypeTarget;
 	
@@ -33,11 +27,6 @@ public class RestrictionHandler implements InvocationHandler {
 		if(r != null) {
 			List<RoleLed> requiredRoles = Arrays.asList(r.value());
 			List<RoleLed> userRoles = new ArrayList<RoleLed>(AccountContext.getAccount().getRoles());
-						
-			List<String> lis1 = requiredRoles.stream().map(rr -> rr.toString()).collect(Collectors.toList());
-			List<String> lis2 = userRoles.stream().map(rr -> rr.toString()).collect(Collectors.toList());
-			
-			LOGGER.info("["+lis1.toString()+"]"+AccountContext.getAccount().getUsername()+"["+lis2.toString()+"]");
 			
 			boolean canPerform = true;
 			for(RoleLed role : requiredRoles) {
